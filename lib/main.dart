3c4b6306ffdd554dc896b256f89b86053521c9f2
Minhal-Ali <minhala9@hotmail.com>
2020-12-04 16:31:39 +0500

    Add StateFull Widget

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,13 @@ void main() {
   ));
 }
 
-class NinjaCard extends StatelessWidget {
+class NinjaCard extends StatefulWidget {
+  @override
+  _NinjaCardState createState() => _NinjaCardState();
+}
+
+class _NinjaCardState extends State<NinjaCard> {
+  int ninjaLevel = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,6 +23,15 @@ class NinjaCard extends StatelessWidget {
         centerTitle: true,
         backgroundColor: Colors.grey[850],
         elevation: 0.0,
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            ninjaLevel += 1;
+          });
+        },
+        child: Icon(Icons.add),
+        backgroundColor: Colors.grey[800],
       ),
       body: Padding(
         padding: EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0.0),
@@ -32,8 +47,7 @@ class NinjaCard extends StatelessWidget {
             Divider(
               height: 60.0,
               color: Colors.grey[600],
-            )
-            ,
+            ),
             Text(
               'Name',
               style: TextStyle(color: Colors.grey, letterSpacing: 2.0),
@@ -60,7 +74,7 @@ class NinjaCard extends StatelessWidget {
               height: 10.0,
             ),
             Text(
-              '8',
+              '$ninjaLevel',
               style: TextStyle(
                   color: Colors.amberAccent[200],
                   letterSpacing: 2.0,
